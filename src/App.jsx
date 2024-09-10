d49f363f6e02b0b62b26ff3857772629f517b5eb
Tushar Bhardwaj <tusharbhardwaj2617@gmail.com>
2024-09-10 11:07:10 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import Background from "./Components/Background/Background";
 
 const App = () => {
@@ -6,12 +7,26 @@ const App = () => {
     {text1:"Indulge",text2:"your passions"},
     {text1:"Give in to",text2:"your passions"},
   ]
-  const [heroCount,setHeroCount]=useState(2);
+  const [heroCount,setHeroCount]=useState(0);
   const [playStatus,setPlayStatus]=useState(false);
+
+  useEffect(()=>{
+    setInterval(()=>{
+      setHeroCount((count)=>{return count===2?0:count+1})
+    },3000)
+  },[])
 
   return (
     <div>
       <Background playStatus={playStatus} heroCount={heroCount}/>
+      <Navbar />
+      <Hero 
+      setPlayStatus={setPlayStatus}
+      heroData={heroData[heroCount]}
+      heroCount={heroCount}
+      setHeroCount={setHeroCount}
+      playStatus={playStatus}
+      />
     </div>
   )
 }
